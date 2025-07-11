646ff41edbea233f5991afbe52c32b0342b11a7d
Grace Manning <gracebmanning@gmail.com>
2025-07-11 12:01:07 -0700

    update layout methods, create audio input ops

--- scripts/automated_text_layouts.py
+++ scripts/automated_text_layouts.py
@@ -12,9 +12,10 @@ from helpers.find_transcript_groupings import find_grouping_times, find_grouping
 # -----------------
 # USER PARAMETERS
 # -----------------
-# File paths for the transcript and word groupings
+# File paths for the transcript, word groupings, audio
 transcript_filename = "../input_files/attentionIsAllYouNeed1_transcript.json"
 groupings_filename = "../input_files/attentionIsAllYouNeed1_groupings.json"
+audio_filename = "../input_files/attentionIsAllYouNeed1_audio.mp3"
 
 # Font styling
 FONT_NAME = "Bahnschrift"
@@ -38,6 +39,77 @@ LAST_FRAME = me.time.end  # Last frame of the
 # -----------------
 # SCRIPT LOGIC
 # -----------------
+
+
+def import_audio():
+    """
+    Creates audiofilein CHOP, plus info CHOP, audioAnalysis and audiodevout CHOP.
+    """
+    # Audio File In
+    audio_file = parent.create(td.audiofileinCHOP, "audio_file")
+    audio_file.nodeX -= BASE_SPACING * 2
+    audio_file.nodeY = 300
+    audio_file.viewer = True
+    audio_file.par.file = audio_filename
+    audio_file.par.playmode = 0
+
+    # Device Out, Audio Info
+    audio_out = parent.create(td.audiodeviceoutCHOP, "audio_out")
+    audio_out.nodeX -= BASE_SPACING
+    audio_out.nodeY = 450
+    audio_out.viewer = True
+    audio_out.inputConnectors[0].connect(audio_file.outputConnectors[0])
+    audio_info = parent.create(td.infoCHOP, "audio_info")
+    audio_info.nodeX -= BASE_SPACING
+    audio_info.nodeY = 350
+    audio_info.viewer = True
+    audio_info.setInputs([audio_file])
+
+    # Audioreactive Scale group
+    audioreactive_scale = parent.create(td.baseCOMP, "audioreactive_scale")
+    audioreactive_scale.nodeX -= BASE_SPACING
+    audioreactive_scale.nodeY = 200
+    in1 = audioreactive_scale.create(td.inCHOP, "in1")
+    in1.viewer = True
+
+    # Math1
+    math1 = audioreactive_scale.create(td.mathCHOP, "math1")
+    math1.setInputs([in1])
+    math1.nodeX = 200
+    math1.viewer = True
+    math1.par.chanop = 5  # Combine Channels = Average
+
+    # Audio Spectrum
+    audiospect1 = audioreactive_scale.create(
+        td.audiospectrumCHOP, "audiospect1")
+    audiospect1.setInputs([math1])
+    audiospect1.nodeX = 400
+    audiospect1.viewer = True
+
+    # Analyze
+    analyze1 = audioreactive_scale.create(td.analyzeCHOP, "analyze1")
+    analyze1.setInputs([audiospect1])
+    analyze1.nodeX = 600
+    analyze1.viewer = True
+
+    # Math 2
+    math2 = audioreactive_scale.create(td.mathCHOP, "math2")
+    math2.setInputs([analyze1])
+    math2.nodeX = 800
+    math2.viewer = True
+    math2.par.torange1 = 0.8  # set To Range to (0.8-1)
+
+    # Null / Out
+    null1 = audioreactive_scale.create(td.nullCHOP, "null1")
+    null1.setInputs([math2])
+    null1.nodeX = 1000
+    null1.viewer = True
+    out1 = audioreactive_scale.create(td.outCHOP, "out1")
+    out1.setInputs([null1])
+    out1.nodeX = 1200
+    out1.viewer = True
+
+    audioreactive_scale.setInputs([audio_file])
 
 
 def create_text_layouts():
@@ -81,13 +153,16 @@ def create_text_layouts():
         processed_lines.append(current_line)
 
         # Create and configure the Layout TOP inside the Base COMP
-        if (item['animation_style'] == 'group_basic'):
-            basic_layout(parent, base, processed_lines, item['options'])
-        else:
-            rectangular_fit_layout(
-                parent, base, processed_lines, item['options'])
+        make_layout(item['animation_style'], parent, base,
+                    processed_lines, item['options'])
 
-        base.setInputs([background])
+        # Set the background
+        base.inputConnectors[0].connect(background.outputConnectors[0])
+
+        # Set audioreactive input
+        if ("audioreactive_scale" in item['options']):
+            base.inputConnectors[1].connect(
+                op('audioreactive_scale').outputConnectors[0])
 
 
 def setup_animation():
@@ -128,7 +203,7 @@ def setup_animation():
         ['index', 1, 'hold', 'hold', 0, 'keys', 0.3, 0.14, 0.7, 0, 0, 0])
 
     # 4. Create a Null CHOP to hold the index channel value
-    index_out = parent.create(td.nullCHOP, "index_out")
+    index_out = parent.create(td.nullCHOP, "main_index_out")
     index_out.viewer = True
     node_x_start += BASE_SPACING
     index_out.nodeX = node_x_start
@@ -162,19 +237,70 @@ def create_group_animations():
 # -----------------
 
 
-def basic_layout(parent, base, processed_lines, options):
+def make_layout(type, parent, base, processed_lines, options):
     # Internal position for operators inside Base COMP
     internal_node_x = 0
     internal_node_y = 0
 
     # Create in TOP for background
-    in_top = base.create(td.inTOP, f"in1")
+    in_top = base.create(td.inTOP, "in1")
     in_top.viewer = True
     in_top.nodeY = internal_node_y
+    internal_node_y -= TOP_SPACING
     internal_node_x += TOP_SPACING
     in_top.nodeX = internal_node_x
-    internal_node_y -= TOP_SPACING
 
+    # Create in CHOP for audioreactive scale (if applicable)
+    if ("audioreactive_scale" in options):
+        in_chop = base.create(td.inCHOP, "in2")
+        in_chop.viewer = True
+        in_top.nodeY = internal_node_y
+        internal_node_y -= TOP_SPACING
+
+    layout = None
+    if (type == 'group_basic'):
+        layout, internal_node_x, internal_node_y = group_basic_layout(
+            parent, base, processed_lines, internal_node_x, internal_node_y)
+    elif (type == 'group_rectangular'):
+        layout, internal_node_x, internal_node_y = group_rectangular_fit_layout(
+            parent, base, processed_lines, internal_node_x, internal_node_y)
+    elif (type == 'group_swirl'):
+        layout, internal_node_x, internal_node_y = group_swirl(
+            parent, base, processed_lines, internal_node_x, internal_node_y)
+    elif (type == 'word_basic'):
+        layout, internal_node_x, internal_node_y = word_basic(
+            parent, base, processed_lines, internal_node_x, internal_node_y)
+    elif (type == 'word_impact'):
+        layout, internal_node_x, internal_node_y = word_impact(
+            parent, base, processed_lines, internal_node_x, internal_node_y)
+    else:
+        pass
+
+    # Create a composite and level TOP
+    comp_top = base.create(td.compositeTOP, "comp1")
+    comp_top.setInputs([in_top, layout])
+    comp_top.par.operand = 0
+    internal_node_x += TOP_SPACING
+    comp_top.nodeX = internal_node_x
+
+    level_top = base.create(td.levelTOP, "level1")
+    if ("invert_colors" in options):
+        level_top.par.invert = 1
+    level_top.setInputs([comp_top])
+    internal_node_x += TOP_SPACING
+    level_top.nodeX = internal_node_x
+
+    # Create an Out TOP and connect Level TOP as Input
+    out = base.create(td.outTOP, "out1")
+    out.viewer = True
+    out.setInputs([level_top])
+    internal_node_x += TOP_SPACING
+    out.nodeX = internal_node_x
+
+    return
+
+
+def group_basic_layout(parent, base, processed_lines, internal_node_x, internal_node_y):
     # Create a Text TOP for each processed line
     num_strings_in_list = len(processed_lines)
     all_text_tops = []
@@ -205,85 +331,173 @@ def basic_layout(parent, base, processed_lines, options):
     # Connect all Text TOPs to the Layout TOP
     layout.setInputs(all_text_tops)
 
-    # Create a composite and level TOP
-    comp_top = base.create(td.compositeTOP, "comp1")
-    comp_top.setInputs([in_top, layout])
-    comp_top.par.operand = 0
+    return layout, internal_node_x, internal_node_y
+
+
+def group_rectangular_fit_layout(parent, base, processed_lines, internal_node_x, internal_node_y):
+    # Create a Text TOP for each processed line
+    num_strings_in_list = len(processed_lines)
+    all_text_tops = []
+    for j, text_line in enumerate(processed_lines):
+        text_top = base.create(td.textTOP, f"text{j}")
+        text_top.viewer = True
+        text_top.par.text = text_line
+        text_top.par.font = FONT_NAME
+        text_top.par.typeface = FONT_TYPEFACE
+        text_top.par.fontsizex = FONT_SIZE
+        text_top.par.resolutionw = parent.par.w
+        text_top.par.resolutionh = parent.par.h / num_strings_in_list
+        text_top.nodeX = internal_node_x
+        text_top.nodeY = internal_node_y
+        internal_node_y -= TOP_SPACING
+        all_text_tops.append(text_top)
+
+    layout = base.create(td.layoutTOP, "layout")
+    layout.viewer = True
+    layout.par.resolutionw = parent.par.w
+    layout.par.resolutionh = parent.par.h
+    layout.par.scaleres = 1  # Scale Resolution to Fit
+    layout.par.align = 3     # Align: Top to Bottom
+    layout.par.fit = 3       # Fit: Fit Best
     internal_node_x += TOP_SPACING
-    comp_top.nodeX = internal_node_x
+    layout.nodeX = internal_node_x
 
-    level_top = base.create(td.levelTOP, "level1")
-    if ("invert_colors" in options):
-        level_top.par.invert = 1
-    level_top.setInputs([comp_top])
+    # Connect all Text TOPs to the Layout TOP
+    layout.setInputs(all_text_tops)
+
+    return layout, internal_node_x, internal_node_y
+
+
+def group_swirl(parent, base, processed_lines, internal_node_x, internal_node_y):
+    # Create a Text TOP for each processed line
+    num_strings_in_list = len(processed_lines)
+    all_text_tops = []
+    for j, text_line in enumerate(processed_lines):
+        text_top = base.create(td.textTOP, f"text{j}")
+        text_top.viewer = True
+        text_top.par.text = text_line
+        text_top.par.font = FONT_NAME
+        text_top.par.typeface = FONT_TYPEFACE
+        text_top.par.fontsizex = FONT_SIZE
+        text_top.par.resolutionw = parent.par.w
+        text_top.par.resolutionh = parent.par.h / num_strings_in_list
+        text_top.nodeX = internal_node_x
+        text_top.nodeY = internal_node_y
+        internal_node_y -= TOP_SPACING
+        all_text_tops.append(text_top)
+
+    layout = base.create(td.layoutTOP, "layout")
+    layout.viewer = True
+    layout.par.resolutionw = parent.par.w
+    layout.par.resolutionh = parent.par.h
+    layout.par.scaleres = 1  # Scale Resolution to Fit
+    layout.par.align = 3     # Align: Top to Bottom
+    layout.par.fit = 3       # Fit: Fit Best
     internal_node_x += TOP_SPACING
-    level_top.nodeX = internal_node_x
+    layout.nodeX = internal_node_x
 
-    # Create an Out TOP and connect Layout TOP as Input
-    out = base.create(td.outTOP, "out1")
-    out.viewer = True
-    out.setInputs([level_top])
+    # Connect all Text TOPs to the Layout TOP
+    layout.setInputs(all_text_tops)
+
+    return layout, internal_node_x, internal_node_y
+
+
+def word_basic(parent, base, processed_lines, internal_node_x, internal_node_y):
+    # Create a Text TOP for each word in the line
+    all_text_tops = []
+    for j, text_line in enumerate(processed_lines):
+        text_top = base.create(td.textTOP, f"text{j}")
+        text_top.viewer = True
+        text_top.par.text = text_line
+        text_top.par.font = FONT_NAME
+        text_top.par.typeface = FONT_TYPEFACE
+        text_top.par.fontsizex = FONT_SIZE
+        text_top.par.resolutionw = parent.par.w
+        text_top.par.resolutionh = parent.par.h
+        text_top.nodeX = internal_node_x
+        text_top.nodeY = internal_node_y
+        internal_node_y -= TOP_SPACING
+        all_text_tops.append(text_top)
+
+    # Create a switch between all text inputs
+    switch = base.create(td.switchTOP, "switch")
+    switch.viewer = True
+    switch.par.resolutionw = parent.par.w
+    switch.par.resolutionh = parent.par.h
     internal_node_x += TOP_SPACING
-    out.nodeX = internal_node_x
+    switch.nodeX = internal_node_x
+    switch.nodeY = internal_node_y
 
-    return
+    # Connect all Text TOPs to the Switch TOP
+    switch.setInputs(all_text_tops)
 
-
-def rectangular_fit_layout(parent, base, processed_lines, options):
-    # Internal position for operators inside Base COMP
-    internal_node_x = 0
-    internal_node_y = 0
-
-    # Create in TOP for background
-    in_top = base.create(td.inTOP, f"in1")
-    in_top.viewer = True
-    in_top.nodeY = internal_node_y
+    # Create animation COMP and add timing
+    animation = base.create(td.animationCOMP, "switch_anim")
     internal_node_x += TOP_SPACING
-    in_top.nodeX = internal_node_x
-    internal_node_y -= TOP_SPACING
+    switch.nodeX = internal_node_x
+    switch.nodeY = internal_node_y
+    # Configure the 'index' channel in the Animation COMP's CHOP data
+    channels_dat = animation.op('channels')
+    channels_dat.clear(keepFirstRow=True)
+    channels_dat.appendRow(
+        ['index', 1, 'hold', 'hold', 0, 'keys', 0.3, 0.14, 0.7, 0, 0, 0])
+    # Configure the keys table in the Animation COMP
+    keys_dat = animation.op('keys')
+    keys_dat.clear(keepFirstRow=True)
+    keys_dat.appendRow([1, 1, 0, 0, 0, "constant()", 0, 0]
+                       )  # Append first row at Frame 1
+    # Find words in transcript and add timings
+    # for i, item in enumerate(matched_timings):
+    # start_frame = item["start_time"] * FPS
+    # keys_dat.appendRow([1, start_frame, i, 0, 0, "constant()", 0, 0])
+    # Append last row at last_frame
+    keys_dat.appendRow([1, LAST_FRAME, 0, 0, 0, "constant()", 0, 0])
 
-    ## PLACEHOLDER - CHANGE LATER ##
-    text_top = base.create(td.textTOP, f"text0")
-    text_top.viewer = True
-    text_top.par.text = "TEMP"
-    text_top.par.font = FONT_NAME
-    text_top.par.typeface = FONT_TYPEFACE
-    text_top.par.fontsizex = FONT_SIZE
-    text_top.par.resolutionw = parent.par.w
-    text_top.par.resolutionh = parent.par.h
-    text_top.nodeX = internal_node_x
-    text_top.nodeY = internal_node_y
-    internal_node_y -= TOP_SPACING
-
-    # Create a composite and level TOP
-    comp_top = base.create(td.compositeTOP, "comp1")
-    comp_top.setInputs([in_top, text_top])
-    comp_top.par.operand = 0
+    # create null for index
+    index_out = base.create(td.nullCHOP, "index_out")
     internal_node_x += TOP_SPACING
-    comp_top.nodeX = internal_node_x
+    index_out.nodeX = internal_node_x
+    index_out.nodeY -= internal_node_y
+    index_out.inputConnectors[0].connect(animation.outputConnectors[0])
 
-    level_top = base.create(td.levelTOP, "level1")
-    if ("invert_colors" in options):
-        level_top.par.invert = 1
-    level_top.setInputs([comp_top])
+    # Connect the animation to the Switch TOP's index parameter
+    switch.par.index.expr = f"op('{index_out.name}')['index']"
+
+    return switch, internal_node_x, internal_node_y
+
+
+def word_impact(parent, base, processed_lines, internal_node_x, internal_node_y):
+    # Create a Text TOP for each processed line
+    num_strings_in_list = len(processed_lines)
+    all_text_tops = []
+    for j, text_line in enumerate(processed_lines):
+        text_top = base.create(td.textTOP, f"text{j}")
+        text_top.viewer = True
+        text_top.par.text = text_line
+        text_top.par.font = FONT_NAME
+        text_top.par.typeface = FONT_TYPEFACE
+        text_top.par.fontsizex = FONT_SIZE
+        text_top.par.resolutionw = parent.par.w
+        text_top.par.resolutionh = parent.par.h / num_strings_in_list
+        text_top.nodeX = internal_node_x
+        text_top.nodeY = internal_node_y
+        internal_node_y -= TOP_SPACING
+        all_text_tops.append(text_top)
+
+    layout = base.create(td.layoutTOP, "layout")
+    layout.viewer = True
+    layout.par.resolutionw = parent.par.w
+    layout.par.resolutionh = parent.par.h
+    layout.par.scaleres = 1  # Scale Resolution to Fit
+    layout.par.align = 3     # Align: Top to Bottom
+    layout.par.fit = 3       # Fit: Fit Best
     internal_node_x += TOP_SPACING
-    level_top.nodeX = internal_node_x
+    layout.nodeX = internal_node_x
 
-    # Create an Out TOP and connect Layout TOP as Input
-    out = base.create(td.outTOP, "out1")
-    out.viewer = True
-    out.setInputs([level_top])
-    internal_node_x += TOP_SPACING
-    out.nodeX = internal_node_x
+    # Connect all Text TOPs to the Layout TOP
+    layout.setInputs(all_text_tops)
 
-    # Create an Out TOP and connect Layout TOP as Input
-    out = base.create(td.outTOP, "out1")
-    out.viewer = True
-    out.setInputs([text_top])
-    internal_node_x += TOP_SPACING
-    out.nodeX = internal_node_x
-
-    return
+    return layout, internal_node_x, internal_node_y
 
 # -----------------
 # BACKGROUNDS
@@ -299,6 +513,7 @@ def background_one(parent):
     noise.par.gain = 1.34
     noise.par.exp = 2.72
     noise.par.mono = 0
+    noise.par.tz.expr = "me.time.frame/100"
     noise.par.resolutionw = parent.par.w
     noise.par.resolutionh = parent.par.h
     noise.nodeX = -BASE_SPACING
@@ -310,16 +525,18 @@ def background_one(parent):
 # -----------------
 # Clear previous components if they exist to allow for rerunning the script
 print("Clearing old components...")
-old_components = []
-old_components.extend(parent.findChildren(name="group*"))
-old_components.extend(parent.findChildren(name="mainSwitch"))
-old_components.extend(parent.findChildren(name="mainAnimation"))
-old_components.extend(parent.findChildren(name="index_out"))
-old_components.extend(parent.findChildren(name="bg*"))
+excluded_ops = ['script1', 'script2']
+old_components = parent.findChildren(maxDepth=1)
 for op_object in old_components:
-    op_object.destroy()
+    if op_object.name in excluded_ops:
+        pass
+    else:
+        op_object.destroy()
 
 # Run the main functions
+print("Importing & analyzing audio...")
+import_audio()
+
 print("Creating text layouts...")
 create_text_layouts()
 
